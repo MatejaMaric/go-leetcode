0aa7a712bebe86ee7e9da90cabf99eb1f103373d
Mateja <mail@matejamaric.com>
2024-01-24 22:51:41 +0100

    Second recursive approach

--- main.go
+++ main.go
@@ -51,3 +51,34 @@ func IsValid(s string) bool {
 
 	return len(stack) == 0
 }
+
+func RecursiveIsValid(runes []rune, stack []rune) bool {
+	if len(runes) == 0 {
+		return len(stack) == 0
+	}
+
+	r := runes[0]
+
+	if r == OpenParenthesis || r == OpenBracket || r == OpenBrace {
+		stack = append(stack, r)
+		return RecursiveIsValid(runes[1:], stack)
+	}
+
+	if len(stack) == 0 {
+		return false
+	}
+
+	head := stack[len(stack)-1]
+
+	if r == CloseParenthesis && head != OpenParenthesis {
+		return false
+	}
+	if r == CloseBracket && head != OpenBracket {
+		return false
+	}
+	if r == CloseBrace && head != OpenBrace {
+		return false
+	}
+
+	return RecursiveIsValid(runes[1:], stack[:len(stack)-1])
+}
--- main_test.go
+++ main_test.go
@@ -23,3 +23,23 @@ func TestIsValid(t *testing.T) {
 		})
 	}
 }
+
+func TestRecursiveIsValid(t *testing.T) {
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{"()", true},
+		{"()[]{}", true},
+		{"(]", false},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("Test_%d", i), func(t *testing.T) {
+			got := RecursiveIsValid([]rune(test.input), make([]rune, 0))
+			if got != test.want {
+				t.Errorf("for string '%s' expected '%t' got '%t'", test.input, test.want, got)
+			}
+		})
+	}
+}
