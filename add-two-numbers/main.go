959264e629193dba5a5bb71e7e064337433e5a18
Mateja <mail@matejamaric.com>
2024-12-17 00:37:09 +0100

    added iterative solution to add two numbers

--- add-two-numbers/main.go
+++ add-two-numbers/main.go
@@ -24,7 +24,7 @@ func AddTwoNumbers(l1 *ListNode, l2 *ListNode, carry int) *ListNode {
 		}
 	}
 
-  retCarry := 0
+	retCarry := 0
 	val := l1.Val + l2.Val + carry
 
 	if val >= 10 {
@@ -36,6 +36,55 @@ func AddTwoNumbers(l1 *ListNode, l2 *ListNode, carry int) *ListNode {
 		Val:  val,
 		Next: AddTwoNumbers(l1.Next, l2.Next, retCarry),
 	}
+}
+
+func IterativeAddTwoNumbers(l1 *ListNode, l2 *ListNode, carry int) *ListNode {
+	var res, tmp *ListNode
+
+	for !(l1 == nil && l2 == nil && carry == 0) {
+		if l1 == nil {
+			l1 = &ListNode{
+				Val:  0,
+				Next: nil,
+			}
+		}
+
+		if l2 == nil {
+			l2 = &ListNode{
+				Val:  0,
+				Next: nil,
+			}
+		}
+
+		retCarry := 0
+		val := l1.Val + l2.Val + carry
+
+		if val >= 10 {
+			val -= 10
+			retCarry = 1
+		}
+
+		if res == nil {
+			res = &ListNode{
+				Val:  val,
+				Next: nil,
+			}
+			l1, l2, carry = l1.Next, l2.Next, retCarry
+
+			tmp = res
+			continue
+		}
+
+		tmp.Next = &ListNode{
+			Val:  val,
+			Next: nil,
+		}
+		l1, l2, carry = l1.Next, l2.Next, retCarry
+
+		tmp = tmp.Next
+	}
+
+	return res
 }
 
 func MakeList(nums []int) *ListNode {
@@ -58,17 +107,17 @@ func ToInt(l *ListNode) int {
 }
 
 func ListsEqual(l1 *ListNode, l2 *ListNode) bool {
-  if l1 == nil && l2 == nil {
-    return true
-  }
-  if l1 == nil && l2 != nil {
-    return false
-  }
-  if l1 != nil && l2 == nil {
-    return false
-  }
-  if l1.Val != l2.Val {
-    return false
-  }
-  return ListsEqual(l1.Next, l2.Next)
+	if l1 == nil && l2 == nil {
+		return true
+	}
+	if l1 == nil && l2 != nil {
+		return false
+	}
+	if l1 != nil && l2 == nil {
+		return false
+	}
+	if l1.Val != l2.Val {
+		return false
+	}
+	return ListsEqual(l1.Next, l2.Next)
 }
--- add-two-numbers/main_test.go
+++ add-two-numbers/main_test.go
@@ -27,6 +27,29 @@ func TestAddTwoNumber(t *testing.T) {
 	})
 }
 
+func TestIterativeAddTwoNumber(t *testing.T) {
+	t.Run("Example 1", func(t *testing.T) {
+		res := ToInt(IterativeAddTwoNumbers(MakeList([]int{2, 4, 3}), MakeList([]int{5, 6, 4}), 0))
+		if res != 807 {
+			t.Errorf("value shoud be 807 but is %d!", res)
+		}
+	})
+
+	t.Run("Example 2", func(t *testing.T) {
+		res := ToInt(IterativeAddTwoNumbers(MakeList([]int{0}), MakeList([]int{0}), 0))
+		if res != 0 {
+			t.Errorf("value shoud be 0 but is %d!", res)
+		}
+	})
+
+	t.Run("Example 3", func(t *testing.T) {
+		res := ToInt(IterativeAddTwoNumbers(MakeList([]int{9, 9, 9, 9, 9, 9, 9}), MakeList([]int{9, 9, 9, 9}), 0))
+		if res != 10009998 {
+			t.Errorf("value shoud be 10009998 but is %d!", res)
+		}
+	})
+}
+
 func TestMakeList(t *testing.T) {
 	res := ToInt(MakeList([]int{7, 0, 8}))
 	if res != 807 {
